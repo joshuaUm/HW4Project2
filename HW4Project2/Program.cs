b40b1e1a552b33a9e32e18b67be4a73405e3fd0f
Josh <Josh@DESKTOP-G13N4EM>
2021-09-21 19:56:53 -0700

    Add project files.

---
+++ HW4Project2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HW4Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numHeads = 0, numTails = 0, numAttempts = 8;
+
+            Console.WriteLine("Coin flip tosses statistics for "+ numAttempts +" attempts.");
+
+            for(int i = 0; i < numAttempts; i++)
+            {
+                while(true) 
+                {
+                   Console.WriteLine("Enter \'h\' or \'t\' #" + (i + 1) + ": ");
+                   String choice = Console.ReadLine();
+
+                   if (choice.CompareTo("h") == 0)
+                   {
+                        numHeads++;
+                        break;
+                   }
+                   else if(choice.CompareTo("t") == 0)
+                   {
+                        numTails++;
+                        break;
+                   }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice, try again.");
+                    }
+                }
+             
+            }
+
+            Console.WriteLine("NumHeads : " + Math.Round((numHeads / (double) numAttempts) * 100 , 1) + "%\n" 
+                            + "NumTails : " + Math.Round((numTails / (double) numAttempts) * 100, 1) + "%");
+
+
+
+        }
+    }
+}
